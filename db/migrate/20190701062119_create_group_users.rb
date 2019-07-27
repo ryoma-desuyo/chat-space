a15ec7f76978331a80412542aaa356484a2f6ca4
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-07-27 22:10:54 +0900

    Update 20190701062119_create_group_users.rb

--- db/migrate/20190701062119_create_group_users.rb
+++ db/migrate/20190701062119_create_group_users.rb
@@ -6,7 +6,7 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-    add_reference :messa    add_reference :messages,:group,foreign_key: true
-ges,:user,foreign_key: true
+    add_reference :messages,:group,foreign_key: true
+    add_reference :messages,:user,foreign_key: true
   end
 end
