9c715b44b2734ae92b3a93f453692191545b0a63
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-07-28 15:52:28 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20190701062119) do
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -51,4 +55,6 @@ ActiveRecord::Schema.define(version: 20190701062119) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
